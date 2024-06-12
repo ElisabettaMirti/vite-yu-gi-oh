fbd36ef47f25db53c5f3720757b785cba45e26f2
ElisabettaMirti <belmirti@gmail.com>
2024-06-12 17:18:33 +0200

    creati i componenti e importati in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <script>
+import AppMain from './components/AppMain.vue';
+import AppHeader from './components/AppHeader.vue';
 
+export default {
+  data(){
+
+  },
+  components: {
+    AppMain,
+    AppHeader
+  }
+}
 </script>
 
 <template>
 
+<AppHeader />
+
+<main>
+  <AppMain />
+</main>
+
+
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  @use './styles/partials/mixins.scss' as *;
+  @use './styles/partials/variables.scss' as *;
+  @use './styles/general.scss';
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+// import 
+
+export default {
+    data(){
+
+    },
+    components: {
+        
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+@use './styles/partials/mixins.scss' as *;
+@use './styles/partials/variables.scss' as *;
+@use './styles/general.scss';
+</style>
